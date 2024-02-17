65a895dd1abf3db11d89a889e5f34f1e04dbc803
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-02-17 13:32:21 -0500

    Added the configuration for ExerciseDbContext

---
+++ Exercise Tracker v2/ExerciseDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Exercise_Tracker_v2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exercise_Tracker;
+
+public class ExerciseDbContext : DbContext
+{
+
+    private readonly IConfiguration _configuration;
+    
+    public ExerciseDbContext(DbContextOptions<ExerciseDbContext> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+        IConfiguration section = _configuration.GetSection("ConnectionStrings");
+
+        string connectionString = section["ExerciseDbConnection"];
+        
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+    
+    public DbSet<Exercise> Exercises { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Exercise>(entity =>
+        {
+            entity.ToTable("Exercises");
+
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd();
+
+            entity.Property(e => e.DateStart).IsRequired();
+            entity.Property(e => e.DateEnd).IsRequired();
+            entity.Property(e => e.Duration).IsRequired();
+            entity.Property(e => e.Comments).HasMaxLength(1000);
+        });
+    }
+}
