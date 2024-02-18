14f05db4db6e31d2531c55f24bbafcc214447108
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-02-17 20:45:36 -0500

    Create ExerciseController.cs

---
+++ Exercise Tracker v2/Controllers/ExerciseController.cs
@@ -0,0 +1,49 @@
+﻿using Exercise_Tracker;
+using Exercise_Tracker_v2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise_Tracker_v2.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class ExerciseController : ControllerBase
+{
+    private readonly IExerciseRepository<Exercise> _exerciseRepository;
+
+    public ExerciseController(IExerciseRepository<Exercise> exerciseRepository)
+    {
+        _exerciseRepository = exerciseRepository;
+    }
+
+    [HttpPost]
+    [Route("AddExercise")]
+    public IActionResult AddExercise(Exercise exercise)
+    {
+        _exerciseRepository.Add(exercise);
+        return Ok();
+    }
+    
+    [HttpPut]
+    [Route("UpdateExercise/{id}")]
+    public IActionResult UpdateExercise(int id, Exercise updatedExercise)
+    {
+        var existingExercise = _exerciseRepository.GetById(id);
+        
+        existingExercise.DateStart = updatedExercise.DateStart;
+        existingExercise.DateEnd = updatedExercise.DateEnd;
+        existingExercise.Duration = updatedExercise.Duration;
+        existingExercise.Comments = updatedExercise.Comments;
+
+        _exerciseRepository.Add(existingExercise);
+        return Ok();
+    }
+    
+    [HttpDelete]
+    [Route("DeleteExercise/{id}")]
+    public IActionResult DeleteExercise(int id)
+    {
+        var existingExercise = _exerciseRepository.GetById(id);
+
+        _exerciseRepository.Delete(existingExercise);
+        return Ok();
+    }
+}
